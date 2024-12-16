702bc6b7dccf445ad88f26c23fc4c27393b62ed4
ornobaadi <aadi4789@gmail.com>
2024-12-16 15:51:21 +0600

    FormData

--- src/shared/Navbar.jsx
+++ src/shared/Navbar.jsx
@@ -19,6 +19,7 @@ const Navbar = () => {
     const links = <>
         <li><NavLink to='/'>Home</NavLink></li>
         <li><NavLink to='/myApplications'>My Applications</NavLink></li>
+        <li><NavLink to='/addJob'>Add a Job</NavLink></li>
     </>
 
     return (
